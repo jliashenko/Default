89bcb4ba3c43823bd9f429126c2ccabb7884bf8a
jliashenko <jliashenko@wageworks.com>
2018-06-27 13:56:34 -0700

    Expanded Greetings Scope

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -10,7 +10,7 @@ namespace Project_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            Console.WriteLine("Hello Solar System.");
         }
     }
 }
